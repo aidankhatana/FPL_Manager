4c18dcd4132ee7a531b1c9f29741fea4062135a0
aidankhatana <98733039+aidankhatana@users.noreply.github.com>
2022-08-05 23:30:46 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from update_team import update
+import asyncio
+import aiohttp
+import os
+from datetime import datetime, timedelta
+import pandas as pd
+from fpl import FPL
+async def check_update():
+    async with aiohttp.ClientSession() as session:
+        fpl = FPL(session)
+        gw = await fpl.get_gameweeks(return_json=True)
+        df = pd.DataFrame(gw)
+        today = datetime.now()
+        tomorrow=(today + timedelta(days=3)).timestamp()
+        today = today.timestamp()
+        df = df.loc[df.deadline_time_epoch>today]
+        deadline = df.iloc[0].deadline_time_epoch
+        return deadline<tomorrow
+
+if __name__ == "__main__":
+    if asyncio.run(check_update()):
+        email=os.environ.get('aidankhatana@icloud.com')
+        password=os.environ.get('PA$5word')
+        user_id=os.environ.get('2793113')
+        asyncio.run(update(email, password,user_id))
